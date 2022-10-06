caa32ba55d2f8123e3809cb4e3769433d650b19e
Ali Salehi <parkour.runner@gmail.com>
2022-10-06 18:41:37 +0300

    homepage done

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import "./css/App.css";
+import Header from "./components/Header";
+import styled from "styled-components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Page from "./pages/Page.jsx";
+import Home from "./pages/Home";
+
+const Container = styled.div`
+  display: flex;
+  background-color: #90d692;
+  direction: rtl;
+`;
+
+const Main = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  background: #e5ffe5;
+  height: inherit;
+  overflow-x: hidden;
+`;
+
+const Wrapper = styled.div`
+  padding: 0 48px;
+  margin: 0 0 48px 0;
+`;
+
+function App() {
+  return (
+    <Container>
+      <BrowserRouter>
+        <Main>
+          <Header />
+          <Wrapper>
+            <Routes>
+              <Route path="/">
+                <Route index element={<Home />} />
+                <Route path="page">
+                  <Route path=":id" element={<Page />} />
+                </Route>
+              </Route>
+            </Routes>
+          </Wrapper>
+        </Main>
+      </BrowserRouter>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/pages/Page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+const Page = () => {};
+
+export default Page;
